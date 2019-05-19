e56971531791e5c8cb09b3e042791753af337d70
BloodyValentine7 <49500411+BloodyValentine7@users.noreply.github.com>
2019-05-20 01:21:24 +0200

    error

--- features_m.py
+++ features_m.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python3
 
 
-def example_m(text, tags):
-	"""Beispielfunktion von Michi"""
-	return "Beispiel M"
+chrono_list = ["also","anfangs","anno","bald","beizeiten","bekanntlich","bereits","bisher","bislang","dadrauf","dadurch","daher","damals","danach","damit","dann","darauf","daraufhin","davor","dazwischen","demnach","demnächst",
+               "dereinst","derweil","doch","drauf","eben","ehemals","einmal","einst","einstmals","einstweilen","erwartungsgemäß","ferner","folglich","früher","gerade","gleich","grad","gleichwohl","infolgedessen","indes","jedoch",
+               "jüngst","just","künftig","kürzlich","längst","letztens","letztlich","letzthin","letztmals","neulich","schließlich","seitdem","seither","sodann","somit","später","späterhin","vordem","vorgestern","gestern","vorher",
+               "vorhin","vormals","weiter","weiters","wodurch","wogegen","womit","wonach","zeither","zuerst","zugleich","zuletzt","überdies"]
+
+
+#basic functions
+def tokenizeFile(readyToTokenize):
+        fixedExpressions = ['Mr.', 'etc.', 'Mrs.', 'Ms.', 'Dr.']
+        returnList = []
+        currentWord = ''
+        for character in readyToTokenize:
+            if currentWord+character in fixedExpressions:
+                returnList.append(currentWord+character)
+                currentWord = ''
+            elif character == '.' or character == ',' or character == '!' or character == '?':
+                returnList.append(currentWord)
+                returnList.append(character)
+                currentWord = ''
+            elif character.isspace():
+                if len(currentWord) > 0:
+                    returnList.append(currentWord)
+                    currentWord = ''
+            else:
+                currentWord += character
+        return returnList
+
+        
+
+#features
+def chronologically_structured(text, tags):
+      
+	
+	
 
 
 
